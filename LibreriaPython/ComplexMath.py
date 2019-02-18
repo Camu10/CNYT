c21501379c41cc94afefe85a19fb399e03b7f693
Carlos <manuel.murillo.1999@gmail.com>
2019-02-17 21:14:04 -0500

    Capitulo2

--- LibreriaPython/ComplexMath.py
+++ LibreriaPython/ComplexMath.py
@@ -1,23 +1,30 @@
 def suma(c1,c2):
     return (c1[0] + c2[0] , c1[1]+c2[1])
-
 def resta(c1,c2):
     return (c1[0] - c2[0] , c1[1] - c2[1])
-    
 def producto(c1,c2):
     return (c1[0]*c2[0]- c1[1]*c2[1] , c1[0]*c2[1] + c2[0]*c1[1])
-
 def division(c1,c2):
     divisor = (c1[1]**2) + (c2[1]**2)
     dividendo1 = c1[0]*c1[1] + c2[0]*c2[1]
     dividendo2 = c1[1]*c2[0] - c1[0]*c2[1]
     return (dividendo1/divisor,dividendo2/divisor)
+def sumaMatrices(mat1,mat2):
+    res = [[] for _ in range(len(mat1))]
+    for i in range(len(mat1)):
+        for j in range(len(mat1[i])):
+            res[i].append(mat1[i][j] + mat2[i][j])
+    return res
+def restaVectores(mat1,mat2):
+    res = []
+    for i in range(len(mat1)):
+        res.append(mat1[i] - mat2[i])
+    return res
 def sumaVectores(mat1,mat2):
     res = []
     for i in range(len(mat1)):
         res.append((mat1[i][0]+mat2[i][0],mat1[i][1]+mat2[i][1]))
     return res
-    
 def inversoVector(mat):
     res = []
     for i in range(len(mat)):
@@ -40,7 +47,7 @@ def conjugadoMatriz(mat):
         for j in range(len(mat[i])):
             res[i].append((mat[i][j][0],-mat[i][j][1]))
     return res
-def productoMatrices(mat1,mat2):
+def productoMatricesComplejas(mat1,mat2):
     res=[[] for _ in range(len(mat1))]
     for i in range(len(mat1)):
         for j in range(len(mat1[i])):
@@ -48,4 +55,58 @@ def productoMatrices(mat1,mat2):
             for k in range(len(mat2)):
                 c = suma(c,producto(mat1[i][k],mat2[k][j]))
             res[i].append(c)
+    return res
+def productoMatrices(mat1,mat2):
+    res=[[] for _ in range(len(mat1))]
+    for i in range(len(mat1)):
+        for j in range(len(mat2)):
+            c = 0
+            for k in range(len(mat2)):
+                c += (mat1[i][k])*(mat2[k][j])
+            res[i].append(c)
+    return res
+def productoInterno(mat1,mat2):
+    trans = transpuesta(mat1)
+    res = productoMatrices(trans,mat2)
+    return res
+def normComplex(mat):
+    res = 0
+    for i in range(len(mat)):
+        res += mat[i][0]**2
+        res += mat[i][1]**2
+    res = res **0.5
+    return res
+def normVector(mat):
+    res = 0
+    for i in range(len(mat)):
+        res += mat[i]**2
+    res = res **0.5
+    return res
+def distanciaVectores(mat1,mat2):
+    a = restaVectores(mat1,mat2)
+    res = normVector(a)
+    return res
+def matrizHermitiana(mat):
+    trans = transpuesta(mat)
+    res = conjugadoMatriz(trans)
+    return res==mat
+def matrizUnitaria(mat):
+    trans = transpuesta(mat)
+    res1 = conjugadoMatriz(trans)
+    res = productoMatricesComplejas(mat,res1)
+    return res
+def productoTensor(mat1,mat2):
+    for x in range(2,len(mat2)**2):
+        if len(mat2) % x == 0:
+            aux = x
+            break
+    res = [[] for _ in range(len(mat1)*len(mat2))]
+    for i in range(len(mat1)):
+        for l in range(len(mat1[i])):
+            a = mat1[i][l]
+            con = i * aux
+            for j in range(len(mat2)):
+                for k in range(len(mat2[j])):
+                    res[con].append(producto(a,mat2[j][k]))
+                con+=1
     return res
--- LibreriaPython/Pruebas.py
+++ LibreriaPython/Pruebas.py
@@ -2,7 +2,7 @@ import unittest
 import Complex
 
 class Pruebas(unittest.TestCase):
-    
+
     def test_suma(self):
         c1 = (3,-1)
         c2 = (1,4)
@@ -40,8 +40,42 @@ class Pruebas(unittest.TestCase):
     def test_producto_matrices(self):
         mat1=[[(3,2),(0,0),(5,-6)],[(1,0),(4,2),(0,1)],[(4,-1),(0,0),(4,0)]]
         mat2=[[(5,0),(2,-1),(6,-4)],[(0,0),(4,5),(2,0)],[(7,-4),(2,7),(0,0)]]
-        resultado = Complex.productoMatrices(mat1,mat2)
+        resultado = Complex.productoMatricesComplejas(mat1,mat2)
         self.assertEqual(resultado,[[(26, -52), (60, 24), (26, 0)], [(9, 7), (1, 29), (14, 0)], [(48, -21), (15, 22), (20, -22)]])
 
+    def test_producto_interno(self):
+        mat1 = [[1,2],[0,1]]
+        mat2 = [[0,-1],[-1,0]]
+        mat3 = [[2,1],[1,3]]
+        resultado = Complex.productoInterno(Complex.sumaMatrices(mat1,mat2),mat3)
+        self.assertEqual(resultado,[[1,-2],[3,4]])
+
+    def test_norm(self):
+        mat = [(4,3),(6,-4),(12,-7),(0,13)]
+        resultado = Complex.normComplex(mat)
+        self.assertEqual(resultado,20.952326839756964)
+
+    def test_norm_vector(self):
+        mat1 = [3,1,2]
+        mat2 = [2,2,-1]
+        resultado = Complex.distanciaVectores(mat1,mat2)
+        self.assertEqual(resultado,3.3166247903554)
+
+    def test_hermitiana_matriz(self):
+        mat = [[(5,0),(4,5),(6,-16)],[(4,-5),(13,0),(7,0)],[(6,16),(7,0),(-2.1,0)]]
+        resultado = Complex.matrizHermitiana(mat)
+        self.assertTrue(resultado)
+
+    def test_unitaria_matriz(self):
+        mat = [[(1,1),(1,-1)],[(1,-1),(1,1)]]
+        resultado = Complex.matrizUnitaria(mat)
+        self.assertEqual(resultado,[[(4,0),(0,0)],[(0,0),(4,0)]])
+
+    def test_producto_tensorial(self):
+        mat1 = [[(3,2),(5,-1),(0,2)],[(0,0),(12,0),(6,-3)],[(2,0),(4,4),(9,3)]]
+        mat2 = [[(1,0),(3,4),(5,-7)],[(10,2),(6,0),(2,5)],[(0,0),(1,0),(2,9)]]
+        resultado = Complex.productoTensor(mat1,mat2)
+        self.assertEqual(resultado,[[(3, 2), (1, 18), (29, -11), (5, -1), (19, 17), (18, -40), (0, 2), (-8, 6), (14, 10)], [(26, 26), (18, 12), (-4, 19), (52, 0), (30, -6), (15, 23), (-4, 20), (0, 12), (-10, 4)], [(0, 0), (3, 2), (-12, 31), (0, 0), (5, -1), (19, 43), (0, 0), (0, 2), (-18, 4)], [(0, 0), (0, 0), (0, 0), (12, 0), (36, 48), (60, -84), (6, -3), (30, 15), (9, -57)], [(0, 0), (0, 0), (0, 0), (120, 24), (72, 0), (24, 60), (66, -18), (36, -18), (27, 24)], [(0, 0), (0, 0), (0, 0), (0, 0), (12, 0), (24, 108), (0, 0), (6, -3), (39, 48)], [(2, 0), (6, 8), (10, -14), (4, 4), (-4, 28), (48, -8), (9, 3), (15, 45), (66, -48)], [(20, 4), (12, 0), (4, 10), (32, 48), (24, 24), (-12, 28), (84, 48), (54, 18), (3, 51)], [(0, 0), (2, 0), (4, 18), (0, 0), (4, 4), (-28, 44), (0, 0), (9, 3), (-9, 87)]])
+        
 if __name__ == '__main__':
     unittest.main()
